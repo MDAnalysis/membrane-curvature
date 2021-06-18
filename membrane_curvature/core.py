bf6019b1966896228ac96e97bec5c75b72ed5480
Estefania Barreto-Ojeda <estefania.barretooje@ucalgary.ca>
2021-06-18 16:06:59 -0600

    Clean code. [Issue #26] (#29)
    * Removed all unused/unnecessary lines.

--- membrane_curvature/core.py
+++ membrane_curvature/core.py
@@ -18,65 +18,9 @@ version = 0.1
 
 sys.path.append('lib/')
 
-
-class Grid():
-    # instance variables
-    def __init__(self, box_width, max_width, unit_cell_width, skip):
-        self.box_width = box_width
-        self.max_width = max_width
-        self.unit_cell_width = unit_cell_width
-        self.skip = skip
-        self.n_cells = math.ceil(max_width / unit_cell_width * 10)
-
-
-class head_indexes:
-    # class variables
-    leaflets = ['lower', 'upper']
-
-    # instance variables
-    def __init__(self, lipid_types, head_index, top):
-        self.lipid_types = lipid_types
-        self.head_index = head_index
-        self.top = top
-
-    # @classmethod
-    def list_head_beads(self):
-        lfs = self.leaflets
-        return def_all_beads(self.lipid_types, lfs, self.head_index, self.top)
-
-
 def main():
 
     start_time = time.time()
-
-    # 1. Parse input files
-    parser = input_options()
-
-    args = parser.parse_args()
-    grofile, trjfile = args.f, args.x
-    unit_width, jump = args.uw, args.sk
-    output = args.out
-
-    try:
-        name_user = str(args.name)
-        print("Prefix assigned by user. Set as {}".format(name_user))
-
-    except NameError:
-        name_user = 'system'
-        print("Prefix not assigned by user. Set as {}".format(name_user))
-
-    # Define index leaflets
-    ii, jj = parse_range(args.io), parse_range(args.ii)
-    iil, iiu = def_range_leaflets(args.io, 1)
-    jjl, jju = def_range_leaflets(args.ii, 2)
-
-    # 2 --- Set OUTPUT files
-    # 2.1 Create output folder
-    os.makedirs(os.path.dirname('output/'), exist_ok=True)
-
-    # 2.2 -- Pickle files (head group height)
-    prefix = name_user
-    name_ = output + prefix
 
     # 3. Define leaflets and lipid type
     leaflets = ['lower', 'upper']
