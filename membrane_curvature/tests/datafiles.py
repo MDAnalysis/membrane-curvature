c7f3276f2bd1959f46c5d8ac6211bcb591b53f84
ojeda-e <estefania.barretooje@ucalgary.ca>
2021-06-20 17:45:50 -0600

    pep8 fixed

--- membrane_curvature/tests/datafiles.py
+++ membrane_curvature/tests/datafiles.py
@@ -10,13 +10,14 @@ MD simulations files stored in `data` sub-directory.
 """
 
 __all__ = [
-    # From lower to higher complexity 
-    "GRO_PO4_SMALL", # Gromacs file of PO4 beads in POPC POPE membrane with 10 lipids
-    "GRO_PO4_INVERTED_ID", # Gromacs test file of PO4 beacs in POPC POPE membrane with 10 lipids with indexes inverted.
-    "GRO_PO4_MED", # Gromacs file of PO4 beads in POPC POPE membrane with 25 lipids
-    "GRO_PO4_BIG", # Gromacs file of PO4 beads in POPC POPE membrane with 50 lipids
-    "GRO_PO4", # Gromacs file of PO4 beads in POPC POPE membrane with 914 lipids
-    "XTC_PO4", # Gromacs trajectory of GRO_PO4 
+    # From lower to higher complexity
+    "GRO_PO4_SMALL",  # Gromacs file of PO4 beads in POPC POPE membrane with 10 lipids
+    # Gromacs test file of PO4 beacs in POPC POPE membrane with 10 lipids with indexes inverted.
+    "GRO_PO4_INVERTED_ID",
+    "GRO_PO4_MED",  # Gromacs file of PO4 beads in POPC POPE membrane with 25 lipids
+    "GRO_PO4_BIG",  # Gromacs file of PO4 beads in POPC POPE membrane with 50 lipids
+    "GRO_PO4",  # Gromacs file of PO4 beads in POPC POPE membrane with 914 lipids
+    "XTC_PO4",  # Gromacs trajectory of GRO_PO4
     "GRO_MEMBRANE_PROTEIN",  # Gromacs file of POPC POPE CHOL membrane
     "XTC_MEMBRANE_PROTEIN",  # Gromacs trajectory of 10 frames.
 ]
@@ -29,10 +30,10 @@ XTC_MEMBRANE_PROTEIN = resource_filename(__name__, '../data/test_curvature_abca1
 # PO4 beads only
 GRO_PO4 = resource_filename(__name__, '../data/test_curvature_po4_only.gro')
 XTC_PO4 = resource_filename(__name__, '../data/test_curvature_po4_only.xtc')
-## Smaler systems derived from
+# Smaler systems derived from
 GRO_PO4_SMALL = resource_filename(__name__, '../data/test_po4_small.gro')
 GRO_PO4_MED = resource_filename(__name__, '../data/test_po4_med.gro')
 GRO_PO4_BIG = resource_filename(__name__, '../data/test_po4_big.gro')
-## Inverted indexes
+# Inverted indexes
 GRO_PO4_INVERTED_ID = resource_filename(__name__, '../data/test_po4_inverted_indexes.gro')
 del resource_filename
