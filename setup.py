4d0817295aa096f089d96b1f124c3740affda906
ojeda-e <estefania.barretooje@ucalgary.ca>
2022-10-25 15:43:45 -0600

    Trial mda 220

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     # url='http://www.my_package.com',  # Website
     install_requires=[
         'numpy>=1.20.0',
-        'mdanalysis>=2.0.0'
+        'mdanalysis>=2.2.0'
     ],
 
     # packages required to run tests
