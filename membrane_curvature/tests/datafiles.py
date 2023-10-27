7d9b6bee73934c6bb74cf22a5a4a94d8bfe49ad3
IAlibay <ialibay@mdanalysis.org>
2023-10-27 12:35:24 +0100

    fix relative path issues

--- membrane_curvature/tests/datafiles.py
+++ membrane_curvature/tests/datafiles.py
@@ -28,19 +28,19 @@ from importlib import resources
 _data_ref = resources.files('membrane_curvature.data')
 
 # Membrane protein systems
-GRO_MEMBRANE_PROTEIN = (_data_ref / '../data/test_curvature_abca1.gro')
-XTC_MEMBRANE_PROTEIN = (_data_ref / '../data/test_curvature_abca1.xtc')
+GRO_MEMBRANE_PROTEIN = (_data_ref / 'test_curvature_abca1.gro')
+XTC_MEMBRANE_PROTEIN = (_data_ref / 'test_curvature_abca1.xtc')
 # PO4 beads only
-GRO_PO4 = (_data_ref / '../data/test_curvature_po4_only.gro')
-XTC_PO4 = (_data_ref / '../data/test_curvature_po4_only.xtc')
+GRO_PO4 = (_data_ref / 'test_curvature_po4_only.gro')
+XTC_PO4 = (_data_ref / 'test_curvature_po4_only.xtc')
 # big systems
-GRO_PO4_SMALL = (_data_ref / '../data/test_po4_small.gro')
-XTC_PO4_SMALL = (_data_ref / '../data/test_po4_small.xtc')
+GRO_PO4_SMALL = (_data_ref / 'test_po4_small.gro')
+XTC_PO4_SMALL = (_data_ref / 'test_po4_small.xtc')
 # membrane-only
-MEMB_GRO = (_data_ref / '../data/MEMB_traj_short.gro')
-MEMB_XTC = (_data_ref / '../data/MEMB_traj_short.xtc')
+MEMB_GRO = (_data_ref / 'MEMB_traj_short.gro')
+MEMB_XTC = (_data_ref / 'MEMB_traj_short.xtc')
 # membrane-protein
-GRO_MEMBPROT_FIT = (_data_ref / '../data/Membrane_protein_fit.gro')
-XTC_MEMBPROT_FIT = (_data_ref / '../data/Membrane_protein_fit.xtc')
+GRO_MEMBPROT_FIT = (_data_ref / 'Membrane_protein_fit.gro')
+XTC_MEMBPROT_FIT = (_data_ref / 'Membrane_protein_fit.xtc')
 
 del resources
