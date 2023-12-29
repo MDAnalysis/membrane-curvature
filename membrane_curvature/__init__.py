de6d4494936cb65ce5fa2b2ae3746d719a983b5f
IAlibay <ialibay@mdanalysis.org>
2023-12-29 19:10:03 +0000

    Switch to versioningit

--- membrane_curvature/__init__.py
+++ membrane_curvature/__init__.py
@@ -15,12 +15,6 @@ from .surface import normalized_grid, derive_surface, get_z_surface
 from .curvature import mean_curvature, gaussian_curvature
 from .base import MembraneCurvature
 
-# Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
-
-from . import _version
-__version__ = _version.get_versions()['version']
+# Handle version
+from importlib.metadata import version
+__version__ = version("membrane_curvature")
